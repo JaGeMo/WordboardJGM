b8d6462ca0ac910894efe64ceedd0ba72561b160
Daniel Wehrle <daniel.wehrle@haufe-lexware.com>
2018-02-19 15:17:00 +0100

    first commit

---
+++ CCWordBoard/Models/Question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCWordBoard.Models
+{
+    public class Question
+    {
+        public Question(List<string> linesOfQuestion)
+        {
+            var answers = new List<String>();
+
+            foreach (var line in linesOfQuestion)
+            {
+                if (line.StartsWith("?"))
+                {
+                    QuestionText = line.Substring(1);
+                } else
+                {
+                    answers.Add(line);
+                }
+            }
+            Answers = answers;
+        }
+
+        public String QuestionText { get; set; }
+
+        public IEnumerable<String> Answers { get; set; }
+
+        
+    }
+}
