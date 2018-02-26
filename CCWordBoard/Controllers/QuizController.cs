b22b9285f6cd40f2da4fe4b9a7506e1b90c2cf18
Jan Moennig <Jan.Moennig@haufe-lexware.com>
2018-02-26 16:39:16 +0100

    initial dev commit

---
+++ CCWordBoard/Controllers/QuizController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CCWordBoard.Controllers
+{
+    public class QuizController : Controller
+    {
+        // read file content and show all questions
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // read file content and show all questions
+        [HttpGet]
+        public IActionResult SelectAnswers()
+        {
+            return View();
+        }
+        
+        // process answers
+//        [HttpPost]
+//        public IActionResult SelectAnswers()
+//        {
+//            return RedirectToAction("QuizTest"); 
+//        }
+
+        // return result string for output
+//        [HttpPost]
+//        public IActionResult QuizTest()
+//        {
+//            return Json( )    
+//        }
+        
+    }
+}
